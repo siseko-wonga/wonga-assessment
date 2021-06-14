37dc2f509594a3fd89378cfac61872d35b5c899e
sisekoreggie <85905404+siseko-wonga@users.noreply.github.com>
2021-06-14 22:51:36 +0200

    Added receive and send console applications

---
+++ Receive/Receive.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "messageQueue",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var receivedMessage = Encoding.UTF8.GetString(body);
+
+                    if (String.IsNullOrWhiteSpace(receivedMessage)) {
+                        Console.WriteLine("Empty message received!!");
+                    } else if (receivedMessage.Length < 19) {
+                        Console.WriteLine("Incorrect message received!!");
+                    } else {
+                        var name = receivedMessage.Substring(18) ;
+                        Console.WriteLine($"Hello {name}, I am your father!");
+                    }
+                };
+                channel.BasicConsume(queue: "messageQueue",
+                                    autoAck: true,
+                                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name on the next line and press [Enter].");
+            string name = Console.ReadLine();
+            string message = $"Hello my name is, {name}";
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                                channel.QueueDeclare(queue: "messageQueue",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                            
+                            var body = Encoding.UTF8.GetBytes(message);
+
+                            channel.BasicPublish(exchange: "",
+                                                routingKey: "messageQueue",
+                                                basicProperties: null,
+                                                body: body);
+                            Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
