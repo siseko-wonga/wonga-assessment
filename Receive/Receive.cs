1d55d11aab3d9e5b78908cd8c987f5f5aa573121
sisekoreggie <85905404+siseko-wonga@users.noreply.github.com>
2021-06-16 15:07:52 +0200

    Format file

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -5,42 +5,47 @@ using System.Text;
 
 namespace Receive
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      var factory = new ConnectionFactory() { HostName = "localhost" };
+      using (var connection = factory.CreateConnection())
+      using (var channel = connection.CreateModel())
+      {
+        channel.QueueDeclare(queue: "messageQueue",
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using(var connection = factory.CreateConnection())
-            using(var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: "messageQueue",
-                                    durable: false,
-                                    exclusive: false,
-                                    autoDelete: false,
-                                    arguments: null);
+          var body = ea.Body.ToArray();
+          var receivedMessage = Encoding.UTF8.GetString(body);
 
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body.ToArray();
-                    var receivedMessage = Encoding.UTF8.GetString(body);
+          if (String.IsNullOrWhiteSpace(receivedMessage))
+          {
+            Console.WriteLine("Empty message received!!");
+          }
+          else if (receivedMessage.Length < 19)
+          {
+            Console.WriteLine("Incorrect message received!!");
+          }
+          else
+          {
+            var name = receivedMessage.Substring(18);
+            Console.WriteLine($"Hello {name}, I am your father!");
+          }
+        };
+        channel.BasicConsume(queue: "messageQueue",
+                            autoAck: true,
+                            consumer: consumer);
 
-                    if (String.IsNullOrWhiteSpace(receivedMessage)) {
-                        Console.WriteLine("Empty message received!!");
-                    } else if (receivedMessage.Length < 19) {
-                        Console.WriteLine("Incorrect message received!!");
-                    } else {
-                        var name = receivedMessage.Substring(18) ;
-                        Console.WriteLine($"Hello {name}, I am your father!");
-                    }
-                };
-                channel.BasicConsume(queue: "messageQueue",
-                                    autoAck: true,
-                                    consumer: consumer);
-
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
-            }
-        }
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+      }
     }
+  }
 }
